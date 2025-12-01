9c2539ce1176716cfc6602765997f7e1e4579b7c
muhammedinshad30-web <muhammedinshad30@gmail.com>
2025-12-01 04:55:54 -0500

    Time: 12 ms (22.73%), Space: 65.7 MB (11.97%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,20 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
- let a=x;
-    let arr=[];
-    while (a > 0 ) {
-        arr.push(a%10)
-        a=Math.floor(a/10)
-    }
-    let b=x.toString()
-    let c=arr.join("")
-    if(b===c){
-        return true
-    }else if(x===0){
-        return true
-    }
-    else{
-       return false
-    }
-}
+    let a=String(x).split("").reverse().join("")
+    return a==x
+};
